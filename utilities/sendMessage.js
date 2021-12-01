6178005d1367d0d3ba39b5c1e52467470ccb8fd4
Fadly <73419981+fngadiyo@users.noreply.github.com>
2021-12-01 14:33:21 +0700

    add dotenv for sensitive variables

--- utilities/sendMessage.js
+++ utilities/sendMessage.js
@@ -1,8 +1,7 @@
 import superagent from 'superagent'
-import { PAGE_ACCESS_TOKEN } from '../config/fbToken.js'
 
 export const sendMessage = (recipientId, text) => {
-    return superagent.post(`https://graph.facebook.com/v12.0/me/messages?access_token=${PAGE_ACCESS_TOKEN}`)
+    return superagent.post(`https://graph.facebook.com/v12.0/me/messages?access_token=${process.env.PAGE_ACCESS_TOKEN}`)
         .type('application/json')
         .send({
             "messaging_type": "RESPONSE",
