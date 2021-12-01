f8fb3d0acae0d1e132527337c005d8bca11d5d4f
Fadly <73419981+fngadiyo@users.noreply.github.com>
2021-12-01 18:44:05 +0700

    define dotenv in necessary place

--- connectionPool.js
+++ connectionPool.js
@@ -1,5 +1,7 @@
+import dotenv from 'dotenv'
 import Sequelize from 'sequelize'
 
+dotenv.config()
 console.log(process.env.DB_HOST, process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, 'credentials')
 
 const connectionPool = new Sequelize({
